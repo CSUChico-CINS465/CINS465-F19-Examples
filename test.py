ae63a4613ff24d457be05dafd5e8987163d6482c
Bryan Dixon <javawolfpack@gmail.com>
2019-09-05 10:42:28 -0700

    Getting started w/ Python Day 1

---
+++ test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+print("Hello World")
+
+x=3
+x="c"
+x=[1,2,3]
+x=[1,"2",3]
+def x(x):
+    print(x)
+#
+# x(x)
+
+y="y"
+x=3
+z = str(x) + y
+print(z)
+
+x = [1,2,3]
+x += [4]
+x.append(5)
+
+y = (1,2,3)
+x+=[y]
+# y += (4)
+# print(x[5][2])
+z = {"key":"value","list":x, "tuple":y}
+print(z)
+# print(x[2])
+# print(y[2])
