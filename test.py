10027b1a70f23fb0c712ce8b3a395cd8237d709c
Bryan Dixon <javawolfpack@gmail.com>
2019-09-10 10:44:40 -0700

    Finishing Python & Getting Started w/ Django

--- test.py
+++ test.py
@@ -22,9 +22,25 @@ x.append(5)
 
 y = (1,2,3)
 x+=[y]
-# y += (4)
+try:
+    y += (4)
+except:
+    print("Tuple's are immutable")
 # print(x[5][2])
 z = {"key":"value","list":x, "tuple":y}
 print(z)
 # print(x[2])
 # print(y[2])
+
+class MyClass:
+    """A simple example class"""
+    def __init__(self):
+        self.data = []
+        self.i = 12345
+
+    def f(self, blah, alice=4, bob=5):
+        # self.bob= 4
+        return 'hello world ' + str(blah) + " " + str(alice) + " " + str(bob)
+
+x = MyClass()
+print(x.f(bob=1, blah="hi"))
