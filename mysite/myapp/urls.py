97f86b77f97d43524375a49fe00d8bc523cb239c
Bryan Dixon <javawolfpack@gmail.com>
2019-09-17 10:40:25 -0700

    Templates and static files

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('<int:page>/', views.index),
     path('', views.index),
+    
+
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("hello world")
+def index(request,page=0):
+    value=range(10*page+10)
+    context={
+        "variable":"Hello World",
+        "title":"Index",
+        "some_list":value[page*9:(page*9+9)]
+    }
+    return render(request, "index.html", context=context)
