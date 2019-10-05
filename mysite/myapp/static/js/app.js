978014eb5434c6bccbf0a231d0e001c601672bf6
Bryan Dixon <javawolfpack@gmail.com>
2019-10-05 15:02:08 -0700

    Timing code for fun

--- mysite/myapp/static/js/app.js
+++ mysite/myapp/static/js/app.js
@@ -5,7 +5,14 @@ import init, { greet } from '/static/js/my_project.js';
 async function run() {
     
     await init();
+    var t1 = performance.now();
     greet();
+    var t2 = performance.now();
+    console.log(t2-t1);
+    var t1 = performance.now();
+    alert("HI");
+    var t2 = performance.now();
+    console.log(t2-t1);
 }
 
 run();
